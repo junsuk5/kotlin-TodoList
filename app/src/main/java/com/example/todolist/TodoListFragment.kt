15c556bcbb942921c3a02e800aae39a7c602ffb9
Junsuk Oh <a811219@gmail.com>
2019-06-19 18:22:23 +0900

    껍데기 완성

---
+++ app/src/main/java/com/example/todolist/TodoListFragment.kt
@@ -0,0 +1,30 @@
+package com.example.todolist
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_todo_list.*
+
+class TodoListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_todo_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        add_button.setOnClickListener {
+            findNavController().navigate(R.id.action_todoListFragment_to_addTodoFragment)
+        }
+    }
+
+
+}
